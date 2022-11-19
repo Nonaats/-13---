32bffa327ff8896c8532f4e0dc4c243fe695c7ff
Nona <nona.cxvariashvili@gmail.com>
2022-11-19 12:45:07 +0400

    Initial Commit

---
+++ დავალება 13 - ნონა/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  დავალება 13 - ნონა
+//
+//  Created by Nona on 19.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
